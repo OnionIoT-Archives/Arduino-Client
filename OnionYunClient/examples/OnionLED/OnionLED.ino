924e32f16b997f92cbf4144240a2a922d145735d
Zheng Han <zh@onion.io>
2014-02-14 21:17:07 -0500

    ethernet client example added

--- OnionYunClient/examples/OnionLED/OnionLED.ino
+++ OnionYunClient/examples/OnionLED/OnionLED.ino
@@ -2,9 +2,8 @@
 #include <Bridge.h>
 #include <YunClient.h> 
 #include <OnionYunClient.h>
-Thermal printer(printer_RX_Pin, printer_TX_Pin, 19200);
 
-OnionYunClient client("d001", "p"); 
+OnionYunClient client("fEXKsE7S", "NfiGGlGMYDO2ZMqF"); 
 
 void lightOn(OnionParams* params) {
   digitalWrite(7, HIGH);
@@ -16,16 +15,12 @@ void lightOff(OnionParams* params) {
 
 void setup() {
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-
   pinMode(7, OUTPUT);
   Bridge.begin();
   client.begin();
   client.get("/on", lightOn);
   client.get("/off", lightOff); 
   
-  digitalWrite(13, LOW);
 }
 
 void loop() {
